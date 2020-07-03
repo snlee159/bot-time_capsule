3faf5f5ab3f84e6b4b74c9bde7babc30597774b8
Sam Lee <sam@Sams-MacBook-Air.local>
2020-07-03 00:17:57 -0700

    Added classes for the channels and the google docs

---
+++ time_capsule/common/channel.py
@@ -0,0 +1,8 @@
+class Channel:
+    def __init__(self, name, channel_id):
+        self.name = name
+        self.channel_id = channel_id
+
+    def get_pins(self):
+        
+        return
---
+++ time_capsule/common/doc.py
@@ -0,0 +1,7 @@
+class Doc:
+    def __init__(self, doc_id):
+        self.doc_id = doc_id
+
+    def write_to_doc(self, message, section):
+
+        return
